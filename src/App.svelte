c116796480ecb98a67aa6bf3b8966ea25974f1d1
Thomas Thoma <tthoma@ots.gr>
2019-09-24 08:05:42 +0300

    Added 'localhost' condition to prevent the serviceworker from managing the cache while we're developing

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,22 @@
 <script>
-  import HelloWorld from './components/HelloWorld.svelte';
-  import Router from './components/Router.svelte';
+  import HelloWorld from "./components/HelloWorld.svelte";
+  import Router from "./components/Router.svelte";
 
-  if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js');
-    }
+  // added 'localhost' condition to prevent serviceworker from managing the cache while we're developing
+  // firefox: go to about:serviceworkers to remove unwanted registered serviceworkers
+  if (
+    "serviceWorker" in navigator &&
+    window.location.hostname !== "localhost"
+  ) {
+    navigator.serviceWorker.register("/service-worker.js");
+  }
 </script>
 
-<style lang="postcss"></style>
+<style lang="postcss">
+
+</style>
 
 <main class="overflow-hidden">
-  <Router/>
-  <HelloWorld/>
+  <Router />
+  <HelloWorld />
 </main>
